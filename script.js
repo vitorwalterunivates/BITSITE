0b4c6485d1cbc7d4220860473efa856928e033f6
vitor-zanatta-walter <vitor.walter@universo.univates.br>
2025-03-21 13:04:59 -0300

    new

--- script.js
+++ script.js
@@ -96,33 +96,34 @@ const enviar = document.getElementById('enviar');
 enviar.addEventListener('click', () => {
 
     let check = true;
+    const colorError = '3px solid rgba(219, 56, 56, 0.6)';
 
     if (nome.value.trim() === '') {
         check = false;
-        nome.style.border = '3px solid rgba(255, 0, 0, 0.6)';
+        nome.style.border = colorError;
     }else{
-        nome.style.border = '0px';
+        nome.style.border = '3px solid rgba(0, 0, 0, 0)';
     }
 
     if (!validarEmail()) {
         check = false;
-        email.style.border = '3px solid rgba(255, 0, 0, 0.6)';
+        email.style.border =  colorError;
     }else{
-        email.style.border = '0px';
+        email.style.border = '3px solid rgba(0, 0, 0, 0)';
     }
 
     if (!validarTelefone()) {
         check = false;
-        telefone.style.border = '3px solid rgba(255, 0, 0, 0.6)';
+        telefone.style.border =  colorError;
     }else{
-        telefone.style.border = '0px';
+        telefone.style.border = '3px solid rgba(0,0,0,0)';
     }
 
     if (senha.value.length < 8) {
         check = false;
-        senha.style.border = '3px solid rgba(255, 0, 0, 0.6)';
+        senha.style.border =  colorError;
     }else{
-        senha.style.border = '0px';
+        senha.style.border = '3px solid rgba(0,0,0,0)';
     }
 
     if (check) {
@@ -133,7 +134,6 @@ enviar.addEventListener('click', () => {
 
 });
 
-
 function validarEmail(){
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email.value);
@@ -142,4 +142,169 @@ function validarEmail(){
 function validarTelefone(){
     const telefoneRegex = /^\d{13}$/;
     return telefoneRegex.test(telefone.value);
+}
+
+
+// JOGO DA VELHA
+
+const botoes_velha = document.getElementsByClassName('botão-velha');
+const btt_reset = document.getElementById('botão-velha-reset')
+let jogadorAtual = 'X';
+let rodada = 0;
+let tabuleiro = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
+btt_reset.addEventListener('click', reset_game);
+
+for (btt in botoes_velha) {
+    botoes_velha[btt].addEventListener('click', bttVelha);
+    botoes_velha[btt].addEventListener('mouseover', bttVelhaHover);
+    botoes_velha[btt].addEventListener('mouseout', bttVelhaHoverOut);
+    botoes_velha[btt].children[0].classList.add('invisivel');
+}
+
+function att_text(text){
+    label = document.getElementById('label-velha');
+    label.textContent = text;
+}
+
+function setTabuleiro(numero,jogador) {
+    const linha = Math.floor(numero / 3);
+    const coluna = numero % 3;
+    tabuleiro[linha][coluna] = jogador;
+}
+
+function posToNumber(pos){
+    let linha = pos[0];
+    let coluna = pos[1];
+    return linha * 3 + coluna;
+}
+
+function mudarJogador() {
+    if (jogadorAtual === 'X') {
+        jogadorAtual = 'O';
+    } else {
+        jogadorAtual = 'X';
+    }
+}
+
+function bttVelhaHover(){
+
+    if (this.disabled === true) {
+        return;
+    }
+
+    this.children[0].classList.remove('invisivel');
+}
+
+function bttVelhaHoverOut(){
+
+    if (this.disabled === true) {
+        return;
+    }
+    
+    this.children[0].classList.add('invisivel');
+}
+
+function bttVelha() {
+    this.disabled = true;
+    setTabuleiro(this.id, jogadorAtual);
+    this.children[0].src = `images/${jogadorAtual}.png`;
+    
+
+    if (checkWin().length > 0) {
+        att_text("Jogador " + jogadorAtual + " venceu!");
+        end();
+        return;
+    }
+
+    if (rodada === 8) {
+        att_text("Empate!");
+        end();
+        return;
+    }
+
+    mudarJogador();
+    changeAllBtts(jogadorAtual);
+    rodada++;
+    att_text(`Jogador atual: ${jogadorAtual}`);
+};
+
+function checkWin() {
+    for (let i = 0; i < 3; i++) {
+        if (tabuleiro[i][0] === tabuleiro[i][1] && tabuleiro[i][1] === tabuleiro[i][2] && tabuleiro[i][0] !== '') {
+            return [[i, 0], [i, 1], [i, 2]];
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (tabuleiro[0][i] === tabuleiro[1][i] && tabuleiro[1][i] === tabuleiro[2][i] && tabuleiro[0][i] !== '') {
+            return [[0, i], [1, i], [2, i]];
+        }
+    }
+
+    if (tabuleiro[0][0] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][2] && tabuleiro[0][0] !== '') {
+        return [[0, 0], [1, 1], [2, 2]];
+    }
+
+    if (tabuleiro[0][2] === tabuleiro[1][1] && tabuleiro[1][1] === tabuleiro[2][0] && tabuleiro[0][2] !== '') {
+        return [[0, 2], [1, 1], [2, 0]];
+    }
+
+    return [];
+}
+
+function changeAllBtts(jogador) {
+    for (btt in botoes_velha) {
+
+        if (botoes_velha[btt].disabled === false) {
+            botoes_velha[btt].children[0].src = `images/${jogador}.png`;
+        }
+    }
+}
+
+function end(){
+
+    let positions = checkWin();
+    let positionsConverted = [];
+
+    for (pos in positions) {
+        positionsConverted.push(posToNumber(positions[pos]));
+    }
+
+    if (positionsConverted.length === 0) {
+        return;
+    }
+
+    for (btt in botoes_velha) {
+
+        botoes_velha[btt].disabled = true;
+
+        if (!positionsConverted.includes(parseInt(botoes_velha[btt].id))) {
+            botoes_velha[btt].children[0].classList.add('invisivel');
+        }       
+    }
+
+
+}
+
+function reset_game(){
+    jogadorAtual = 'X';
+    rodada = 0;
+    tabuleiro = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    
+    ];
+    att_text(`Jogador atual: ${jogadorAtual}`);
+    for (btt in botoes_velha) {
+        botoes_velha[btt].disabled = false;
+        botoes_velha[btt].children[0].src = `images/${jogadorAtual}.png`;
+        botoes_velha[btt].children[0].classList.add('invisivel');
+    }
+    
 }
